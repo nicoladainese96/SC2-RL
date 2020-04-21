995aa62a302b0e6ddca986ac4a73a98e8f21b521
nicoladainese96 <nicola.dainese96@gmail.com>
2020-04-21 17:45:59 +0300

    sampling action parameters - pt1

--- RelationalModule/AC_networks.py
+++ RelationalModule/AC_networks.py
@@ -51,6 +51,96 @@ class OheActor(nn.Module):
         action_mask = ~np.array([self.action_dict[i] in available_actions for i in self.action_dict.keys()])
         return action_mask
     
+class OheParamActor(nn.Module):
+    """
+    Use OheNet followed by a linear layer with log-softmax activation.
+    """
+    def __init__(self, action_space, map_size, env, **net_args):
+        """
+        Parameters
+        ----------
+        action_space: int
+            Number of (discrete) possible actions to take
+        map_size: int
+            If input is (batch_dim, n_channels, linear_size, linear_size), 
+            then map_size = linear_size - 2
+        env: int
+            Instance of the environment
+        **net_args: dict (optional)
+            Dictionary of {'key':value} pairs valid for OheNet
+        """
+        super(OheParamActor, self).__init__()
+        
+        # Environment-related attributes
+        self.action_dict = {0:_NO_OP, 1:_SELECT_ARMY, 2:_MOVE_SCREEN}
+        self.screen_res = env.observation_spec()[0]['feature_screen'][1:]
+        self.all_actions = env.action_spec()[0][1]
+        self.all_arguments = env.action_spec()[0][0]
+        
+        # Networks
+        self.net = net.OheNet(map_size, **net_args)
+        self.linear = nn.Linear(self.net.n_features, action_space)
+        self._init_params_nets()
+    
+    def _init_params_nets(self):
+        self.arguments_networks = {}
+        self.arguments_dict = {}
+
+        for a in action_dict:
+            action = self.all_actions[action_dict[a]]
+            args = action.args
+
+            for arg in args:
+                self.arguments_dict[arg.name] = arg.id # store 'name':id pairs for future use
+                if debug: print('\narg.name: ', arg.name)
+
+                size = self.all_arguments[arg.id].sizes
+                if debug: print('size: ', size)
+                if len(size) == 1:
+                    if debug: print("Init CategoricalNet for "+arg.name+' argument')
+                    self.arguments_networks[arg.name] = net.CategoricalNet(self.net.n_features, size[0]) 
+                else:
+                    if debug: print("Init SpatialNet for "+arg.name+' argument')
+                    self.arguments_networks[arg.name] = net.SpatialNet(self.net.n_features, size) 
+        return
+    
+    def forward(self, state, available_actions):
+        out = self.net(state)
+        logits = self.linear(out)
+        if debug: print("logits: ", logits)
+            
+        mask = self.get_action_mask(available_actions)
+        if debug: print("mask: ", mask)
+            
+        logits[:,mask] = torch.tensor(np.NINF)
+        if debug: print("logits (after mask): ", logits)
+            
+        log_probs = F.log_softmax(logits, dim=-1)
+        return log_probs, out
+     
+    def sample_param(self, state_rep, arg_name):
+        return self.arguments_networks['arg_name'](state_rep)
+    
+    ## Check this !!
+    def get_arguments(self, state_rep, action_id):
+        action = self.all_actions[action_id]
+        list_of_args = action.args
+        
+        args = []
+        log_probs = []
+        for arg in list_of_args:
+            arg_sampled, log_prob, _ = self.sample_param(state_rep, arg.name)
+            args.append(arg_sampled)
+            log_probs.append(log_prob)
+            
+        log_prob = torch.stack(log_probs).sum()
+        
+        return args, log_prob
+        
+    def get_action_mask(self, available_actions):
+        action_mask = ~np.array([self.action_dict[i] in available_actions for i in self.action_dict.keys()])
+        return action_mask
+    
 class OheBasicCritic(nn.Module):
     """
     Use OheNet followed by a linear layer with a scalar output without
