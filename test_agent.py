c7649bad9af650421e0a12d33641a6e21458acd4
nicoladainese96 <nicola.dainese96@gmail.com>
2020-09-03 09:23:22 +0200

    results added to notebook 21

--- test_agent.py
+++ test_agent.py
@@ -1,8 +1,8 @@
 import os
 import numpy as np
 
-names = np.array(['CollectMineralShards%s'%i for i in range(1,6)]+\
-        ['FindAndDefeatZerglings%s'%i for i in range(1,6)])
+names = np.array(['MoveToBeacon%s'%i for i in range(1,6)])#+\
+        #['FindAndDefeatZerglings%s'%i for i in range(1,6)])
 
 for n in names:
     cmd = 'python monobeast.py --map_name %s --mode test --savedir ~/Desktop/Master\ Thesis/SC2-RL/logs/torchbeast/ --xpid %s'%(n[:-1],n)
